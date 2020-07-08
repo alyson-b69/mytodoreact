372fcac731d26d94b0aa3f273e8c5cca6f81b597
alyson-b69 <alyson.bernabeu@gmail.com>
2020-07-08 16:37:16 +0200

    Updates

---
+++ precache-manifest.ffbc7e926a34baf7405b8d79ce6ab762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12eb801dd5e3266e01b38b7f88f10aab",
+    "url": "/mytodoreact/index.html"
+  },
+  {
+    "revision": "b7105aaca600930db859",
+    "url": "/mytodoreact/static/css/main.02621b05.chunk.css"
+  },
+  {
+    "revision": "fdcabf6d6f40e347ec19",
+    "url": "/mytodoreact/static/js/2.80b2fcde.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mytodoreact/static/js/2.80b2fcde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7105aaca600930db859",
+    "url": "/mytodoreact/static/js/main.f2b4fad9.chunk.js"
+  },
+  {
+    "revision": "fe2971b1bbbbd208757c",
+    "url": "/mytodoreact/static/js/runtime-main.9198a2df.js"
+  }
+]);
